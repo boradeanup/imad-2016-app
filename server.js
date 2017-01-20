4ff11338e0463da1d0b0f3c17638bc68d1601769
boradeanup <boradeanup@gmail.com>
2017-01-20 18:28:48 +0530

    server upgraded to handle 2 new URLs

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/article2', function (req, res) {
+  res.send("Article 2 is requested and will be served")
+});
+
+app.get('/articleN', function (req, res) {
+  res.send("Article N is requested and will be served")
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
